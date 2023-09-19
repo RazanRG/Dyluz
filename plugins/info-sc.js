fd3b490efcec87dfa57e772edfe691b546fd512b
razan <razantanvan@gmail.com>
2023-09-19 10:42:33 +0700

    ziro

---
+++ plugins/info-sc.js
@@ -0,0 +1,24 @@
+let handler = async (m) => {
+
+let anu =`
+*❏––––––『 𝐒𝐂𝐑𝐈𝐏𝐓 𝐁𝐎𝐓 』––––––❏*
+
+Pengen Scnya?
+✨SC InI DI JUAL‼️
+Harga ?? Sangat Murah
+
+Minat ?? Chat Wa
+wa : wa.me/6281228070013
+
+⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕
+
+${botdate}
+`
+
+conn.fakeReply(m.chat, anu, '0@s.whatsapp.net', '⚠️ *Sc ori base ziro* ⚠️', 'status@broadcast')
+}
+handler.help = ['script', 'sc']
+handler.tags = ['tools']
+handler.command = /^(sc|script)$/i
+handler.limit = false
+export default handler
